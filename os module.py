db05ee7f7ddbd90d3273aa9cb5eb1ab6af5ab784
Melody Egwuchukwu <melodyegwuchukwu1@gmail.com>
2025-02-04 16:01:45 +0100

    sprint 103 learning

---
+++ os module.py
@@ -0,0 +1,11 @@
+import os
+
+#os.chdir('Hackthon')
+#print(os.listdir()) # list all the file in this working directory
+#os.mkdir('subfolder')
+#print(os.getcwd())
+#os.rmdir('subfolder')
+
+#os.rmdir('subfolder/subsubfolder')
+
+os.rename("romeoandjuliet.txt", "romeo_and_juliet.txt") #renaming
