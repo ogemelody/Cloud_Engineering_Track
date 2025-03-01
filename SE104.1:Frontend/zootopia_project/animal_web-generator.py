cafe9e823ea00c2c9abec569c45be43c389292b6
Melody Egwuchukwu <melodyegwuchukwu1@gmail.com>
2025-03-01 18:03:07 +0100

    generate card item for every animal

--- SE104.1:Frontend/zootopia_project/animal_web-generator.py
+++ SE104.1:Frontend/zootopia_project/animal_web-generator.py
@@ -1,40 +1,49 @@
 import json
 
 
-
 def load_data(file_path):
     """
     :param file_path: animal_data.json
-    :return:Function to load the JSON data from a file
+    :return: Function to load the JSON data from a file
     """
     with open(file_path, "r") as file:
         data = json.load(file)
         return data
 
 
+# Function to serialize a single animal object into HTML
+def serialize_animal(animal_obj):
+    """
+    :param animal_obj: A single animal object
+    :return: A string that contains the HTML list item representation of the animal
+    """
+    output = ''
+    name = animal_obj['name']
+    diet = animal_obj['characteristics']['diet']
+    location = animal_obj['locations'][0]
+    type_value = animal_obj['characteristics'].get('type')
+
+    # append information to each string
+    # Create a list item <li> for each animal
+    output += f"<li class='cards__item'>\n"
+    output += f"<h2 class='card__title'>{name}</h2>\n"
+    output += f"<p class='card__text'>Diet: {diet}</p>\n"
+    output += f"<p class='card__text'>Location: {location}</p>\n"
+    if type_value:
+        output += f"<p class='card__text'>Type: {type_value}</p>\n"
+    output += "</li>\n"  # Close the <li> tag
+
+    return output
+
+
 def attribute(data_file):
     """
     :param data_file: list of animals
-    :return: Function to generate the string with animal information
+    :return: Function to generate the string with animal information by calling serialize_animal
     """
-    animals = data_file
     output = ''
-    for animal in animals:
-        name = animal['name']
-        diet = animal['characteristics']['diet']
-        location = animal['locations'][0]
-        type_value = animal['characteristics'].get('type')
-
-        # append information to each string
-        # Create a list item <li> for each animal
-        output += f"<li class='cards__item'>\n"
-        output += f"<h2 class='card__title'>{name}</h2>\n"
-        output += f"<p class='card__text'>Diet: {diet}</p>\n"
-        output += f"<p class='card__text'>Location: {location}</p>\n"
-        if type_value:
-            output += f"<p class='card__text'>Type: {type_value}</p>\n"
-        output += "</li>\n"  # Close the <li> tag
-
+    for animal in data_file:
+        output += serialize_animal(animal)  # Call the new function to serialize each animal
     return output
 
 
